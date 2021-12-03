10b163de2199edb041a638a701171b55ff14a2c9
EduardoLovo <eduardo.lovo@gmail.com>
2021-12-03 10:32:27 -0300

    mostrando fotos

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import * as C from './App.styles';
+import * as Photos from './Services/photos';
+import { Photo } from './Types/photo';
+import { PhotoItem } from './Components/PhotoItem';
 
-function App() {
+const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(()=>{
+    const getPhotos = async () => {
+      setLoading(true);
+      setPhotos(await Photos.getAll());
+      setLoading(false);
+    }
+    getPhotos();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de Fotos</C.Header>
+
+        {/* Área de upload */}
+
+        {/* Lista de Fotos */}
+        {loading &&
+          <C.ScreenWarning>
+            <div className="emoji">✋</div>
+            <div>Carregando...</div>
+          </C.ScreenWarning>
+        }
+
+        {!loading && photos.length > 0 &&
+          <C.PhotoList>
+            {photos.map((item, index)=>(
+              <PhotoItem key={index} url={item.url} name={item.name} />
+            ))}
+          </C.PhotoList>
+        }
+
+        {!loading && photos.length === 0 &&
+          <C.ScreenWarning>
+            <div className="emoji">📷</div>
+            <div>Não há fotos cadastradas.</div>
+          </C.ScreenWarning>
+        }
+
+      </C.Area>
+    </C.Container>
   );
 }
 
