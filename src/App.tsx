8adfcd9372d2cccfa60efc96beb60a609829cd27
EduardoLovo <eduardo.lovo@gmail.com>
2021-12-03 18:51:30 -0300

    Projeto finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 import * as C from './App.styles';
 import * as Photos from './Services/photos';
 import { Photo } from './Types/photo';
@@ -8,6 +8,7 @@ const App = () => {
 
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState<Photo[]>([]);
+  const [uploading, setUploading] = useState(false);
 
   useEffect(()=>{
     const getPhotos = async () => {
@@ -18,12 +19,47 @@ const App = () => {
     getPhotos();
   }, []);
 
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get('image') as File;
+    
+    if (file && file.size > 0) {
+      setUploading(true);
+        // Faz o upload do arquivo
+        let result = await Photos.insert(file);
+
+      setUploading(false);
+
+      if(result instanceof Error) {
+        alert(`${result.name} - ${result.message}`);
+      } else {
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList);
+      }
+       
+    }
+  }
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Galeria de Fotos</C.Header>
 
         {/* Área de upload */}
+        <C.UploadForm 
+          method="POST"
+          onSubmit={handleFormSubmit}
+        >
+          <input type="file" name="image" />
+          <input type="submit" value="enviar" />
+          {uploading && "Enviando..."}
+
+        </C.UploadForm>
+
 
         {/* Lista de Fotos */}
         {loading &&
